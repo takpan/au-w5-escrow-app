392173949c0f1295d275aa53a57f03c00aa4508d
Panagiotis Panagoulias <ppanagoulias82@gmail.com>
2024-09-15 17:52:33 +0300

    Support contract deployment in Sepolia testnet

--- hardhat.config.cjs
+++ hardhat.config.cjs
@@ -1,8 +1,15 @@
 require('@nomicfoundation/hardhat-toolbox');
+require("dotenv").config();
 
 module.exports = {
   solidity: "0.8.24",
   paths: {
     artifacts: "./app/src/artifacts",
+  },
+  networks: {
+    sepolia: {
+      url: process.env.SEPOLIA_URL,
+      accounts: [process.env.SEPOLIA_PRIVATE_KEY]
+    },
   }
 };
